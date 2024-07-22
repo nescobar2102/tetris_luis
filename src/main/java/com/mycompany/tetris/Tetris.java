b915619294272e7cf00f32e3d3f3e75c97ebc417
Norbelys Naguanagua <norbelysnaguanagua21@gmail.com>
2024-07-22 15:52:00 -0500

    Iniciando proyecto tetris

---
+++ src/main/java/com/mycompany/tetris/Tetris.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tetris;
+ 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Tetris extends JFrame {
+    private JLabel statusbar;
+
+    public Tetris() {
+        initUI();
+    }
+
+    // crea La interfaz grafica
+    private void initUI() {
+        // La puntuacion se muestra en la parte inferior
+        // del tablero
+        statusbar = new JLabel("Puntaje: 0");
+        add(statusbar, BorderLayout.SOUTH);
+
+        // se Crea el tablero del juego y se inicia la partida
+        var tablero = new Tablero(this);
+        add(tablero);
+        tablero.start();
+
+        // muestra el titulo del juego y establece las
+        // dimensiones de la pantalla
+        setTitle("Tetris 2D");
+        setSize(400, 800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    JLabel getStatusBar() {
+        return statusbar;
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            var game = new Tetris();
+            game.setVisible(true);
+        });
+    }
+}
